68158d82e788b7cd1dcba934208ce00930cd4253
MohamedAljia <benaljia.mohamed@esprit-tn.com>
2026-04-23 12:28:44 +0100

    Keep only ApiGatway folder

--- user-service/src/main/java/tn/esprit/userservice/web/exception/BadRequestException.java
+++
@@ -1,7 +0,0 @@
-package tn.esprit.userservice.web.exception;
-
-public class BadRequestException extends RuntimeException {
-    public BadRequestException(String message) {
-        super(message);
-    }
-}
